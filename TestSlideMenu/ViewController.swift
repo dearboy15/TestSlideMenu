4c34634ad235400d8b5d21b60bfa0944afb290d4
Tua Sarunyoo <satoshi.tuat@gmail.com>
2015-08-26 21:59:17 +0900

    Initial Commit

---
+++ TestSlideMenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestSlideMenu
+//
+//  Created by SARUNYOO LAMUNGKUN on 8/26/15.
+//  Copyright (c) 2015 SARUNYOO LAMUNGKUN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
